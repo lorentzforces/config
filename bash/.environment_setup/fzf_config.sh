54784b59849cee0686abbe84462d6479015d8bcc
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2022-01-17 22:42:13 -0600

    add console colors, ripgrep config, some other tweaks

--- bash/.environment_setup/fzf_config.sh
+++ bash/.environment_setup/fzf_config.sh
@@ -45,7 +45,7 @@ function fzf_git_show() {
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
                 (grep -o '\<[a-f0-9]\+\>' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                xargs -I % sh -c 'git show --color=never % | nvim -R -') << 'FZF-EOF'
                 {}
 FZF-EOF"
 }
