0fffa4668056a9578e7d4391359ce24975390fcf
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2022-07-18 14:21:28 -0500

    work machine updates

--- work-machine/.environment_setup/local_profile_setup.sh
+++ work-machine/.environment_setup/local_profile_setup.sh
@@ -3,3 +3,5 @@ export PATH="${brew_prefix}/libexec/gnubin:/usr/local/bin:$PATH"
 export MANPATH="${brew_prefix}/libexec/gnubin:/usr/local/bin:$MANPATH"
 
 export BASH_SILENCE_DEPRECATION_WARNING=1
+
+source $HOME/.environment_setup/session_setup.sh
