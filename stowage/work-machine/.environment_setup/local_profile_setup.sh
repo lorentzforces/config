30534af0692b693496d769cb3ace3181c30e4dac
Lorentz Aberg <25649139+lorentzforces@users.noreply.github.com>
2024-01-09 23:55:11 -0600

    quote some bash values better

--- stowage/work-machine/.environment_setup/local_profile_setup.sh
+++ stowage/work-machine/.environment_setup/local_profile_setup.sh
@@ -3,7 +3,7 @@ export PATH=$(ensure-path "/opt/homebrew/bin")
 
 coreutils_prefix="$(brew --prefix coreutils)"
 export PATH=$(ensure-path "${coreutils_prefix}/libexec/gnubin")
-export MANPATH=$(echo $MANPATH | ensure-path --stdin "${coreutils_prefix}/libexec/gnuman")
+export MANPATH=$(echo "$MANPATH" | ensure-path --stdin "${coreutils_prefix}/libexec/gnuman")
 
 # turns out this was in an inconvenient spot in the PATH
 export PATH=$(ensure-path "/usr/local/bin")
@@ -13,4 +13,4 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 export HELM_LOCAL_VALUES="$HOME/mine/dev-env/helm-tag-values-override.yaml"
 export K9SCONFIG="$HOME/.config/k9s"
 
-source $HOME/.environment_setup/session_setup.sh
+source "$HOME/.environment_setup/session_setup.sh"
