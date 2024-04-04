3d6431321abf70dd403ab8634ba6bedede1e8843
Lorentz Aberg <25649139+lorentzforces@users.noreply.github.com>
2024-04-04 10:09:49 -0500

    add kubeconfig discovery

--- stowage/work-machine/.environment_setup/local_session_setup.sh
+++ stowage/work-machine/.environment_setup/local_session_setup.sh
@@ -2,3 +2,5 @@
 # are not passed into subshells. In other words, this needs to be done at each session startup.
 export SDKMAN_DIR="$HOME/mine/programs/sdkman"
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+export KUBECONFIG=$(find ~/.kube/config* | tr '\n' ':' | sed s/:$//)
