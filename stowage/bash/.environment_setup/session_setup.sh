7493e90fcdb40b7783cede2293838b375002caec
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2024-07-07 08:00:11 -0500

    refactor more bash scripts

--- stowage/bash/.environment_setup/session_setup.sh
+++ stowage/bash/.environment_setup/session_setup.sh
@@ -45,52 +45,34 @@ alias ls='ls --color=auto --group-directories-first --classify --width=90'
 alias la='ls -A --group-directories-first --color=auto'
 alias ll='ls -Alh --group-directories-first --color=auto'
 
-alias g='git'
-
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias cdm='cd $HOME/mine'
 
-function gco()
-{
-	local target
-	target=$(fzf_git_branches)
-
-	if [ -z "$target" ]; then
-		return 1
-	fi
-	git checkout "$target"
-}
-
-function delete-branch()
-{
-	local target
-	target=$(fzf_git_branches)
-
-	if [ -z "$target" ]; then
-		return 1
-	fi
-	confirm-prompt "Are you sure you want to delete branch: $target? " || return 1
-	git branch -d "$target"
-}
+alias g='git'
+alias gco='git-checkout-fuzzy'
+alias fshow='git-log-fuzzy-search'
+alias delete-branch='git-delete-branch-fuzzy'
 
 alias fcd='cd $(fzf_containing_dir)'
 alias fif='fzf_find_in_files'
 
+alias tmuxhere='tmuxp-current-dir'
+alias fdev='tmuxp-session-dev-dirs'
+alias fmux='tmuxp-defined-sessions'
+
 alias page='nvim -R'
 alias pageify='fc -s | nvim -R -'
 alias lessify='fc -s | less -R'
-
 alias restow='stow -R --no-folding'
-
 alias printpath='printenv PATH | sed s/:/\\n/g'
 
 # lf alias with directory following (when lf exits, cd to the directory it was in)
 function fd()
 {
 	local tmp
-	tmp="$(mktemp)"
+	tmp="$(mktemp --tmpdir 'lf_fd.XXXXX')"
 	lf -last-dir-path="$tmp" "$@"
 	if [ -f "$tmp" ]; then
 		local dir
@@ -99,50 +81,6 @@ function fd()
 		if [[ -d "$dir" && "$dir" != "$(pwd)" ]]; then
 			cd "$dir" || { echo "cd to $dir failed" 1>&2; exit 1; }
 		fi
-	fi
-}
-
-function fdev()
-{
-	local dev_dir
-	dev_dir=$(fzf_directories_at "$HOME/mine/repos")
-
-	if [ -z "$dev_dir" ]; then
-		return 1
-	else
-		local dev_name
-		dev_name=$(basename "$dev_dir")
-		DEV_DIR="$dev_dir" DEV_NAME="$dev_name" tmuxp load --yes dev
-	fi
-}
-
-function tmuxhere()
-{
-	local target_dir
-	if [ -n "$1" ]; then
-		target_dir="$1"
-	else
-		target_dir=$(pwd)
-	fi
-
-	local name
-	name=$(basename "$target_dir")
-	SESSION_NAME="$name" SESSION_PATH="$target_dir" tmuxp load --yes "generic-with-path"
-}
-
-function fmux()
-{
-	local tmuxp_file
-	tmuxp_file=$(
-		tmuxp ls \
-		| rg -v "dev|generic-with-path" \
-		| fzf-tmux -u20 -- --no-hscroll --ansi --no-multi
-	)
-
-	if [ -z "$tmuxp_file" ]; then
-		return 1
-	else
-		tmuxp load --yes "$tmuxp_file"
 	fi
 }
 
