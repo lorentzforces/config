cb6eee56c8bad46c4f8b345089e2dd9496a1ac99
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2024-09-19 16:06:51 -0500

    add skel script

---
+++ scripts/skel.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Create my entire directory structure (or verify it's there).
+#
+# Focus here is on directories which I need/expect to exist. This doesn't take any action or make
+# any assertions about the contents of those directories (for example, mine/media is here, but not
+# mine/media/books).
+#
+# All paths are relative to $HOME.
+
+paths=(
+	"mine/programs"
+	"mine/programs/bin"
+	"mine/info"
+	"mine/media"
+	"mine/repos"
+)
+
+function main() {
+	for skel_dir in "${paths[@]}"; do
+		mkdir -p "$skel_dir"
+	done
+}
+
+main
