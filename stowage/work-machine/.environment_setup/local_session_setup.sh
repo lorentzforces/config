dd247a68b6f6c894995d93a05dca086d30a0982d
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2024-11-04 14:33:19 -0600

    iron out some issues with zsh (probably swap back to bash anyway tho)
    zsh is... slow to start up, and I'm not a fan of some of the weird
    interactions with configuration. will probably revert back to bash
    config soon, and will figure out how to make that sane on a mac at some
    other point.

--- stowage/work-machine/.environment_setup/local_session_setup.sh
+++ stowage/work-machine/.environment_setup/local_session_setup.sh
@@ -1,5 +1,10 @@
 # Sdkman init doesn't work in .profile because it initializes itself as a set of functions, which
 # are not passed into subshells. In other words, this needs to be done at each session startup.
+
+# remove existing SDKMAN paths in case we're inheriting the PATH (ex: starting a tmux session)
+# TODO: change ensure-path so we can just delete entries without having to add one
+# PATH=$(ensure-path -d "sdkman/candidates")
+
 export SDKMAN_DIR="$HOME/mine/programs/sdkman"
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
