6c88bedcdd9f3153e51c1e2df716e52136123e05
Lorentz Aberg <25649139+lorentzforces@users.noreply.github.com>
2024-12-11 15:22:54 -0600

    factor out nvim indent settings into a function

--- stowage/neovim/.config/nvim/init.lua
+++ stowage/neovim/.config/nvim/init.lua
@@ -151,10 +151,7 @@ vim.api.nvim_create_user_command(
 	'Tab',
 	function(opts)
 		local indent_size = tonumber(opts.fargs[1])
-		util.options.expandtab = false
-		util.options.tabstop = indent_size
-		util.options.softtabstop = indent_size
-		util.options.shiftwidth = indent_size
+		util.set_indents(true, indent_size)
 	end,
 	{ nargs = 1 }
 )
@@ -163,10 +160,7 @@ vim.api.nvim_create_user_command(
 	'Space',
 	function(opts)
 		local indent_size = tonumber(opts.fargs[1])
-		util.options.expandtab = true
-		util.options.tabstop = indent_size
-		util.options.softtabstop = indent_size
-		util.options.shiftwidth = indent_size
+		util.set_indents(false, indent_size)
 	end,
 	{ nargs = 1 }
 )
