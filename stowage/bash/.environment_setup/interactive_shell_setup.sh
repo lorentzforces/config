757f59a0608df6d531f161cd1788940887f93522
Lorentz Aberg <25649139+lorentzforces@users.noreply.github.com>
2024-12-18 12:32:08 -0600

    add bash completion scripts from homebrew

--- stowage/bash/.environment_setup/interactive_shell_setup.sh
+++ stowage/bash/.environment_setup/interactive_shell_setup.sh
@@ -48,6 +48,29 @@ eval "$(fnm completions --shell bash)"
 PATH=$(ensure-path -d "fnm_multishells" "${FNM_MULTISHELL_PATH}/bin")
 export PATH
 
+### enable programmable completion features
+
+# TODO: verify that this works on OpenSUSE
+if [[ -f /usr/share/bash-completion/bash_completion ]]; then
+	# shellcheck source=/dev/null
+	source "/usr/share/bash-completion/bash_completion"
+fi
+
+if type brew &>/dev/null; then
+	HOMEBREW_PREFIX="$(brew --prefix)"
+	if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+		# shellcheck source=/dev/null
+		source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+	else
+		for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+			# shellcheck source=/dev/null
+			[[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+		done
+	fi
+fi
+
+### aliases
+
 alias ls='ls --color=auto --group-directories-first --classify --width=90'
 alias la='ls -A --group-directories-first --color=auto'
 alias ll='ls -Alh --group-directories-first --color=auto'
@@ -58,9 +81,11 @@ alias ....='cd ../../..'
 alias cdm='cd $HOME_MINE'
 
 alias g='git'
+__git_complete g __git_main
 alias kc='kubectl'
 alias gco='git-checkout-fuzzy'
 alias fshow='git-log-fuzzy-search'
+__git_complete fshow _git_log
 alias delete-branch='git-delete-branch-fuzzy'
 
 alias fcd='cd $(fzf_containing_dir)'
@@ -94,16 +119,6 @@ function fd()
 		fi
 	fi
 }
-
-# TODO: verify what the hell this is supposed to be on macos
-# enable programmable completion features
-if [[ -f /usr/share/bash-completion/bash_completion ]]; then
-	# shellcheck source=/dev/null
-	source "/usr/share/bash-completion/bash_completion"
-elif [[ -f /etc/bash_completion ]]; then
-	# shellcheck source=/dev/null
-	source "/etc/bash_completion"
-fi
 
 ### per-machine configuration
 # we do this last so we can override anything per-machine
