57c16bfae125d496d0c8d304610013e6b8b468ff
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2025-02-09 00:18:10 -0600

    remove outdated stuff from nvim init

--- stowage/neovim/.config/nvim/init.lua
+++ stowage/neovim/.config/nvim/init.lua
@@ -4,17 +4,21 @@ local util = require('stupid-namespace.utils')
 -- taken from https://lazy.folke.io/installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo(
+			{
+				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+				{ out, "WarningMsg" },
+				{ "\nPress any key to exit..." },
+			},
+			true,
+			{}
+		)
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -37,11 +41,6 @@ require('lazy').setup({
 		path = '~/mine/repos',
 	},
 })
-
--- fixed by: https://github.com/neovim/neovim/pull/31676
--- TODO: remove with upgrade to neovim 0.10.4 or later
--- neovim 0.10.3 will throw an error when running :Inspect without this
-vim.hl = vim.highlight
 
 util.options.number = false
 util.options.relativenumber = false
