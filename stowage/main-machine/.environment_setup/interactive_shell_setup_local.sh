fa10e88a9464891c1d6ccd89a6ebd8ad45056cf6
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2025-03-30 00:04:53 -0500

    make "open" actually work

--- stowage/main-machine/.environment_setup/interactive_shell_setup_local.sh
+++ stowage/main-machine/.environment_setup/interactive_shell_setup_local.sh
@@ -1,5 +1,3 @@
-alias open="xdg-open"
-
 # Sdkman init doesn't work in .profile because it initializes itself as a set of functions, which
 # are not passed into subshells. In other words, this needs to be done at each session startup.
 export SDKMAN_DIR="$HOME/mine/programs/sdkman"
