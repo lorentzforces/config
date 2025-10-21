74a1c846642b2ffde2c0e4cb5042bfb786a5a6ea
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2025-10-21 17:02:26 -0500

    colorize topline status

--- stowage/bash/.environment_setup/prompt_cmd_setup.src.sh
+++ stowage/bash/.environment_setup/prompt_cmd_setup.src.sh
@@ -4,15 +4,18 @@
 __prompt_cmd() {
 	local prev_status="$?"
 
+	# NOTE: The bash prompt requires any non-printing characters (like escape sequences) to be
+	# enclosed with square braces for cursor-position-counting reasons, since bash itself has no
+	# idea what characters are printing or not. This is not true in general bash scripts, so the
+	# next time you copy some sequences from here KEEP THAT IN MIND.
+
 	# I have some colors swapped from their typical representations in my typical color scheme
 	# (red is not one of them)
 	local red='\[\033[31m\]'
-	# terminal: purple/5, me: orange
-	local purple='\[\033[35m\]'
-	# terminal: cyan/6, me: purple
-	local cyan='\[\033[36m\]'
+	local purple='\[\033[35m\]' # terminal: purple/5, me: orange
+	local cyan='\[\033[36m\]' # terminal: cyan/6, me: purple
 	local bold='\[\033[1m\]'
-	local reset='\[\033(B\[\033[m\]'
+	local reset='\[\033[0m\]'
 
 	PS1=""
 	PS1+="$bold"
