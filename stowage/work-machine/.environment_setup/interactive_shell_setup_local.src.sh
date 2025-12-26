93c6fda35cad4d5ef2b6f25bf49dd91a85bbb917
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2025-12-26 12:55:07 -0600

    move common functions to namespaced scripts (_ltz) & fix up some bash style

--- stowage/work-machine/.environment_setup/interactive_shell_setup_local.src.sh
+++ stowage/work-machine/.environment_setup/interactive_shell_setup_local.src.sh
@@ -5,10 +5,10 @@ export SDKMAN_DIR="$HOME/mine/programs/sdkman"
 # shellcheck source=/dev/null
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
-work_build_setup="$HOME/mine/dev-env/setup-work-build-env.src.sh"
+work_build_setup="$HOME/mine/dev-env/work-interactive-shell-setup.src.sh"
 if [[ -r "$work_build_setup" ]]; then
 	# shellcheck source=/dev/null
 	source "$work_build_setup"
 else
-	>&2 printf "==SHELL== work build env declaration not found at %s" "$work_build_setup"
+	>&2 printf "==SHELL== work setup script not found at %s" "$work_build_setup"
 fi
