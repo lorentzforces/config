20a375d9ed296bb725eb95cc2205dcc37e6b384a
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2026-02-18 12:13:42 -0600

    clean up some setup scripts

--- scripts/install-sdkman.sh
+++ scripts/install-sdkman.sh
@@ -4,4 +4,16 @@ IFS=$'\n\t'
 
 # One-liner
 
-curl -s "https://get.sdkman.io?rcupdate=false" | SDKMAN_DIR="$HOME/.local/share/sdkman" bash
+DL_PATH="/tmp/install-sdkman-script"
+SDK_INSTALL_URL="https://get.sdkman.io?rcupdate=false"
+
+if [[ -f "$DL_PATH" ]]; then
+	rm "$DL_PATH"
+fi
+
+curl -s "$SDK_INSTALL_URL" >"$DL_PATH" || {
+	>&2 echo "Failed to download sdkman install script from ${SDK_INSTALL_URL}";
+	exit 1;
+}
+
+SDKMAN_DIR="$HOME/.local/share/sdkman" bash -- "$DL_PATH"
