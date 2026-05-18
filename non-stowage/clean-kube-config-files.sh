037f69a02a44096514626f41e5a6e1b19a27d8cd
Lorentz <25649139+lorentzforces@users.noreply.github.com>
2026-05-18 15:56:32 -0500

    scripts: remove debug path-printing from kube cleanup script

--- non-stowage/clean-kube-config-files.sh
+++ non-stowage/clean-kube-config-files.sh
@@ -3,9 +3,6 @@
 # Delete kubeconfig files older than two weeks.
 
 main() {
-
-	>&2 printenv PATH
-
 	local timestamp
 	timestamp=${ date -d '2 weeks ago' '+%s'; }
 	prunejuice --include '\Aconfig_' --since-unix-time "$timestamp" --no-confirm "$HOME/.kube"
